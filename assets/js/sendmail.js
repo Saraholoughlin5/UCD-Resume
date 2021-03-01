adc5ac770a97a737ce3093a7569a154d46ee6e1a
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-03-01 10:05:08 +0000

    implement email integration on contact page

---
+++ assets/js/sendmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("service_a7rogl1", "template_6dev72e", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+
+}
